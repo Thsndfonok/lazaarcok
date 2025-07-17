1ea4c369f7de823c9601cdfe3b6520ab01fd247b
Thsndfonok <fadmivan@gmail.com>
2025-07-17 06:03:40 +0200

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-document.getElementById("enter-btn").addEventListener("click", () => {
-  const container = document.getElementById("fade-container");
-  container.classList.add("fade-out");
-
-  // 1 másodperc után átirányít
-  setTimeout(() => {
-    window.location.href = "main.html";
-  }, 1000);
-});
